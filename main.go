cc84fe39292a683f010099d07285ed56f96a3561
= <victordouglas999@gmail.com>
2022-08-02 21:27:46 -0300

    implementacao api events

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,6 +10,19 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Event struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type allEvents []Event
+
+var events = allEvents{
+	{ID: 1, Title: "Unilão", Description: "Evento 1"},
+	{ID: 2, Title: "Unilão", Description: "Evento 2"},
+}
+
 func main() {
 	// access log
 	log.Println("Starting API...")
@@ -16,6 +30,7 @@ func main() {
 	port := os.Getenv("PORT")
 	router := mux.NewRouter()
 	router.HandleFunc("/", Home)
+	router.HandleFunc("/events", GetAllEvents).Methods("GET")
 	http.ListenAndServe(":"+port, router) // Test heroku
 
 	//Test localhost:8080
@@ -26,6 +41,13 @@ func main() {
 func Home(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Home endpoint accessed")
 	fmt.Fprintf(w, "Aplication is running...")
+}
+
+func GetAllEvents(w http.ResponseWriter, r *http.Request) {
+	log.Println("GettingAllEvents endpoint accessed")
+	w.Header().Set("Content-Type", "Aplication/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(events)
 }
 
 //comandos:
